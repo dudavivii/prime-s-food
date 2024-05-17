4377ea6eacfee4de614743f29416a2023febc01d
13Richard04 <richarddiezaraujo@gmail.com>
2024-05-17 15:37:12 -0300

    Atualização Sprint 2
    Atualização Sprint 2.1.3

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -5,7 +5,7 @@ import { assets } from '../../assets/assets'
 const AppDownload = () => {
   return (
      <div className='app-download' id='app-download'>
-      <p>Para uma Melhor Experiência, baixe nosso App! <br /> Prime Eat's App </p>
+      <p>Para uma Melhor Experiência, baixe nosso App! <br /><b>Prime Eat's App</b></p>
       <div className="app-download-plataforms">
         <img src={assets.play_store} alt="" />
         {/* <img src={assets.app_store} alt="" /> */}
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
                 <div className="footer-social-icons">
                     <img src={assets.facebook_icon} alt=''/>
                     <img src={assets.twitter_icon} alt= ''/>
-                    <img src={assets.linkedin_icon} alt='' />
+                    {/* <img src={assets.linkedin_icon} alt='' /> */}
                 </div>
             </div>
             <div className="footer-content-center">
